434700f698151ad5cc23f88ddfdf7c61043a6a1c
Mauricio-Alonso <conta.alonso@gmail.com>
2022-11-04 17:58:24 -0300

    Hechos algunos ajustes

--- src/components/views/DetalleReceta.jsx
+++ src/components/views/DetalleReceta.jsx
@@ -7,8 +7,9 @@ import { obtenerRecetaAPI } from '../helpers/queries';
 import Swal from "sweetalert2";
 import Spinner from '../common/Spinner';
 
+const DetalleReceta = () =>{ 
 
-function DetalleReceta() {
+const {id} = useParams(); 
 
 const [nombreReceta, setNombreReceta] = useState ();
 const [dificultad, setDificultad] = useState ();
@@ -21,12 +22,12 @@ const [imagen, setImagen] = useState ();
 const [mostrarSpinner, setMostrarSpinner] = useState(false)
 
 // traer el parámetro de la ruta
-const {id} = useParams();
+
 
 
 useEffect(()=>{
-    setMostrarSpinner(true);
-    obtenerRecetaAPI(id).then((respuesta)=>{
+      setMostrarSpinner(true);
+        obtenerRecetaAPI(id).then((respuesta)=>{
       if(respuesta.status===200){
         setNombreReceta(respuesta.dato.nombreReceta)
         setDificultad(respuesta.dato.dificultad)
@@ -72,75 +73,3 @@ const mostrarComponente = (mostrarSpinner === true) ? (<Spinner></Spinner>):(<Ca
 }
 
 export default DetalleReceta;
-
-/*
-
-import React, {  useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
-import {useParams} from "react-router-dom"
-import { obtenerRecetaAPI } from '../helpers/queries';
-import Swal from "sweetalert2";
-
-function DetalleReceta() {
-
-const [nombreReceta, setNombreReceta] = useState ();
-const [dificultad, setDificultad] = useState ();
-const [ingredientes, setIngredientes] = useState ();
-const [categoria, setCategoria] = useState ();
-const [pasos, setPasos] = useState ();
-const [imagen, setImagen] = useState ();
-
-// traer el parámetro de la ruta
-const {id} = useParams();
-
-
-useEffect(()=>{
-    obtenerRecetaAPI(id).then((respuesta)=>{
-      if(respuesta.status===200){
-        setNombreReceta(respuesta.dato.nombreReceta)
-        setDificultad(respuesta.dato.dificultad)
-        setIngredientes(respuesta.dato.ingredientes)
-        setCategoria(respuesta.dato.categoria)
-        setPasos(respuesta.dato.pasos)
-        setImagen(respuesta.dato.imagen)
-      }else{
-      Swal.fire('Ocurrio un error', 'Intente este paso en unos minutos', 'error')
-      }
-    })
-  },[])
-
-
-
-
-  return (
-    <div className="container">
-    <Card style={{display: 'flex', flexDirection: 'row'}}>
-      <Card.Img variant="top  w-25 " src={imagen} />
-      <Card.Body>
-        <Card.Title>{nombreReceta}</Card.Title>
-        <hr></hr>
-        <Card.Text className="my-3 fw-bold">
-        <span className = "m-3" >Dificultad: {dificultad}</span>
-        
-        <Badge bg="success">{categoria}</Badge>
-        </Card.Text>
-        <hr></hr>
-        <Card.Text className="my-3">
-        {ingredientes}
-        </Card.Text>
-        <hr></hr>
-        <Card.Text className="my-3">
-        {pasos}
-        </Card.Text>
-       </Card.Body>
-    </Card>
-    </div>
-  );
-}
-
-export default DetalleReceta;
-
-
-*/
--- src/components/views/Receta/CardReceta.jsx
+++ src/components/views/Receta/CardReceta.jsx
@@ -15,12 +15,12 @@ return (
         <Card.Title>{receta.nombreReceta}</Card.Title>
         <Card.Text className="my-3"><span className="fw-bold">Dificultad: </span>{receta.dificultad}</Card.Text>
         <Card.Text className="my-3"><span className="fw-bold">Categoría: </span> <Badge bg="success">{receta.categoria}</Badge></Card.Text>
-        <hr></hr>
         <Card.Text className="my-3"><span className="fw-bold">Ingredientes: </span>{receta.ingredientes}</Card.Text>
-        <hr></hr>
         <Card.Text className="my-3"><span className="fw-bold">Resumen: </span>{receta.resumen}</Card.Text>
          <hr></hr>
-        <Link className="btn btn-danger w-100" to={`/DetalleReceta/${receta.id}`}>¡Aprendé como prepararlo!</Link>
+        <div className="text-center">
+        <Link className="btn btn-danger " to={`/DetalleReceta/${receta.id}`}>Ver más</Link>
+       </div>
       </Card.Body>
     </Card>
     
